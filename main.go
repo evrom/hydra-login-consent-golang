0597d9e720b6f148262d706ee463054adc99725e
Evan Roman <evanroman1@gmail.com>
2021-12-09 13:10:23 +0200

    implement working api for login flow

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"github.com/gin-gonic/gin"
-
-		"fmt"
 	"net/url"
-  "github.com/ory/hydra-client-go/client"
-  "github.com/ory/hydra-client-go/client/admin"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ory/hydra-client-go/client"
+	"github.com/ory/hydra-client-go/client/admin"
+	"github.com/ory/hydra-client-go/models"
 )
 
 func main() {
@@ -21,23 +22,28 @@ func main() {
 	})
 	router.GET("/login", func(c *gin.Context) {
 		challenge := c.Query("login_challenge")
-		adminURL, _ := url.Parse("http://127.0.0.1:4445")
-		hydraAdmin := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{Schemes: []string{adminURL.Scheme}, Host: adminURL.Host, BasePath: adminURL.Path})
-		result, err := hydraAdmin.Admin.GetLoginRequest(admin.NewGetLoginRequestParams().WithLoginChallenge(challenge))
-		if err != nil {
-			fmt.Printf("%v", err)
-		} else {
-			fmt.Printf("%v", result)
-		}
 		c.HTML(http.StatusOK, "login.html", gin.H{
-			"challenge": "Main website",
+			"challenge": challenge,
 		})
 	})
 
 	router.POST("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.html", gin.H{
-			"challenge": "hi",
-		})
+		challenge := c.PostForm("challenge")
+		adminURL, _ := url.Parse("http://127.0.0.1:4445")
+		hydraAdmin := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{Schemes: []string{adminURL.Scheme}, Host: adminURL.Host, BasePath: adminURL.Path})
+		subject := "foo@bar.com"
+		result, err := hydraAdmin.Admin.AcceptLoginRequest(
+			admin.NewAcceptLoginRequestParams().
+				WithLoginChallenge(challenge).
+				WithBody(&models.AcceptLoginRequest{
+					Subject: &subject,
+				}))
+		if err != nil {
+			fmt.Printf("%v", err)
+			c.Redirect(http.StatusFound, "/login")
+		} else {
+			c.Redirect(http.StatusFound, *result.Payload.RedirectTo)
+		}
 	})
 
 	router.GET("/logout", func(c *gin.Context) {
