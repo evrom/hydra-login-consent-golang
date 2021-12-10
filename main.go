e989ac0b9434f6877e2f96b0fe4681f932c38c2f
Evan Roman <evanroman1@gmail.com>
2021-12-10 07:16:05 +0200

    implement consent page get method

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -14,10 +14,24 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
-	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
+
+	adminURL, _ := url.Parse("http://127.0.0.1:4445")
+	hydraAdmin := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{Schemes: []string{adminURL.Scheme}, Host: adminURL.Host, BasePath: adminURL.Path})
+
 	router.GET("/consent", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+		challenge := c.Query("consent_challenge")
+		result, err := hydraAdmin.Admin.GetConsentRequest(admin.NewGetConsentRequestParams().WithConsentChallenge(challenge))
+		if err != nil {
+			log.Printf("%v", err)
+		} else {
+			log.Printf("%v", result)
+		}
+
+		c.HTML(http.StatusOK, "consent.html", gin.H{
+			"title":           "Main website",
+			"RequestedScopes": result.Payload.RequestedScope,
+			"ClientID":        result.Payload.Client.ClientID,
+			"User":            result.Payload.Subject,
 		})
 	})
 	router.GET("/login", func(c *gin.Context) {
@@ -29,8 +43,6 @@ func main() {
 
 	router.POST("/login", func(c *gin.Context) {
 		challenge := c.PostForm("challenge")
-		adminURL, _ := url.Parse("http://127.0.0.1:4445")
-		hydraAdmin := client.NewHTTPClientWithConfig(nil, &client.TransportConfig{Schemes: []string{adminURL.Scheme}, Host: adminURL.Host, BasePath: adminURL.Path})
 		subject := "foo@bar.com"
 		result, err := hydraAdmin.Admin.AcceptLoginRequest(
 			admin.NewAcceptLoginRequestParams().
@@ -39,7 +51,7 @@ func main() {
 					Subject: &subject,
 				}))
 		if err != nil {
-			fmt.Printf("%v", err)
+			log.Printf("%v", err)
 			c.Redirect(http.StatusFound, "/login")
 		} else {
 			c.Redirect(http.StatusFound, *result.Payload.RedirectTo)
